ba00902854707b19ee24ecd9effc47f3e857201f
Valkon <96625039+Nawija@users.noreply.github.com>
2024-03-29 16:37:24 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pl">
       <head>
-        <meta name="theme-color" content="#080808" />
+        <meta name="theme-color" content="#191919" />
       </head>
       <body className={roboto_serif.className}>
         <Header />
