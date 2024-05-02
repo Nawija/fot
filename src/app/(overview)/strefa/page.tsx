9cf3424d4dc10f8c2fa4ef22aa4b99a0c0fb3017
Valkon <96625039+Nawija@users.noreply.github.com>
2024-05-02 17:57:31 +0200

    Update page.tsx

--- src/app/(overview)/strefa/page.tsx
+++ src/app/(overview)/strefa/page.tsx
@@ -1,9 +1,14 @@
-import Hero from "@/components/hero/Hero";
-
 export default function Strefa() {
   return (
     <div className="anim-opacity">
-      <div className="h-[60vh]"></div>
+      <div className="relative flex h-[27vh] w-full items-center justify-center">
+        <img
+          className="absolute left-0 top-0 -z-10 h-full w-full object-cover object-center"
+          src="https://cdn.pixabay.com/photo/2020/01/02/17/25/champagne-4736380_1280.jpg"
+        />
+        <div className="absolute left-0 top-0 z-10 h-full w-full bg-black/30" />
+        <h1 className="text-4xl font-semibold text-white z-30">Strefa Klienta</h1>
+      </div>
     </div>
   );
 }
