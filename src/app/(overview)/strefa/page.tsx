4e5c13c4f4dd95d7ba341a59c2f3da7c650977f1
Valkon <96625039+Nawija@users.noreply.github.com>
2024-05-02 15:30:08 +0200

    gdf

--- src/app/(overview)/strefa/page.tsx
+++ src/app/(overview)/strefa/page.tsx
@@ -2,8 +2,8 @@ import Hero from "@/components/hero/Hero";
 
 export default function Strefa() {
   return (
-    <>
+    <div className="anim-opacity">
       <div className="h-[60vh]"></div>
-    </>
+    </div>
   );
 }
